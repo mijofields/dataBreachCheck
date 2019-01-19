aa9bb7747dbdc93e37fda2edf54b21345b09edfa
Mike Fields <mikefields81@gmail.com>
2019-01-18 21:03:04 -0600

    pastecheck and formatting complete

--- breach.js
+++ breach.js
@@ -8,15 +8,15 @@ const axios = require('axios');
 const cTable = require('console.table');
 const numeral = require('numeral');
 const BreachTableObject = require('./breachTableObject.js');
+const pasteModelBreach = require('./breachTableObject.js');
 
 
 
 const question1 = chalk.bold.whiteBright("Hola Mike, what are we checking for today??");
 const question2 = chalk.bold.whiteBright("Whats the email address we are checking for??");
+const question3 = chalk.bold.whiteBright("Whats the email address we are checking pastes for??");
 const choice1 = chalk.red("Check email address breaches");
 const choice2 = chalk.yellow("Check email address pastes");
-
-
 
 
 function start() {
@@ -44,7 +44,7 @@ function start() {
 
 
             case choice2:
-                console.log('API call for paste checking');
+                pasteCheck();
                 break;
 
             default:
@@ -131,9 +131,89 @@ function emailCheck() {
 
 
 
-    }); //end of then
+    }); //end of then emailCheck
 
 }; //end of emailCheck
+
+function pasteCheck() {
+
+    inquirer
+
+        .prompt({
+
+            name: "email",
+            type: "input",
+            message: question3,
+            validate: function(answer) {
+
+                    return /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/gi.test(answer);
+
+                } //end of validation
+
+
+
+        }) //end of prompt
+
+    .then(function(answer) {
+
+        axios({
+
+                method: 'get',
+                url: `https://haveibeenpwned.com/api/v2/pasteaccount/${answer.email}`,
+                headers: { 'User-Agent': `Mike's JS CLI` },
+                timeout: 1000,
+
+            })
+            .then(function(response) {
+
+                let tableObjectArr1 = [];
+
+                for (i = 0; i < response.data.length; i++) {
+
+                    let data = response.data[i];
+                    let tableObject1 = new pasteModelBreach(data.Source, data.Title, data.Date, numeral(data.EmailCount).format('0,0'));
+
+                    tableObjectArr1.push(tableObject1);
+
+                }
+
+                console.log(chalk.bold.red(`There have been the following ${response.data.length} breaches of ${answer.email}:`));
+                console.table(tableObjectArr1);
+
+
+
+            })
+            .catch(function(error2) {
+
+                console.log(chalk.underline.yellow(`Lucky you ${answer.email} has no registered pastes!`))
+                console.log('Error: ', error2.message);
+
+            })
+
+
+
+
+        //end of axios call
+
+
+
+
+
+
+
+    }); //end of then emailCheck
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
 start();
--- breachTableObject.js
+++ breachTableObject.js
@@ -10,4 +10,13 @@ function BreachTableObject(name, breachDate, pwnCount, isVerified, isFabricated)
 
 };
 
-module.exports = BreachTableObject;
+function pasteModelBreach(source, date, title, emailCount) {
+
+    this.Source = source,
+        this.Paste_Date = date,
+        this.Title = title,
+        this.Email_Count = emailCount
+
+}
+
+module.exports = BreachTableObject, pasteModelBreach;
